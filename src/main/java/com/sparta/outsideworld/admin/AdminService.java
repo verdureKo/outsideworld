a7b050e0b7d1abbcc1d0f0a50193f1daa1b8f287
shung1103 <shung1103@hanmail.net>
2023-07-21 17:35:10 +0900

    백 오피스 주석 추가

--- src/main/java/com/sparta/outsideworld/admin/AdminService.java
+++ src/main/java/com/sparta/outsideworld/admin/AdminService.java
@@ -22,12 +22,15 @@ public class AdminService {
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
+    // 전체 유저 조회
     public List<User> getUserList() {
         return userRepository.findAll();
     }
 
+    // 전체 게시글 조회
     public List<Post> getPostList() { return postRepository.findAll(); }
 
+    // 유저 프로필 수정
     public void updateUserProfile(Long userid, ProfileRequestDto profileRequestDto) {
         User user = userRepository.findById(userid).orElseThrow(
                 () -> new NullPointerException("해당 유저가 존재하지 않습니다.")
@@ -38,6 +41,7 @@ public class AdminService {
         userRepository.save(user);
     }
 
+    // 유저 권한 User 로 변경
     public void updateUserRole(Long userid) {
         User user = userRepository.findById(userid).orElseThrow(
                 () -> new NullPointerException("해당 유저가 존재하지 않습니다.")
@@ -46,6 +50,7 @@ public class AdminService {
         userRepository.save(user);
     }
 
+    // 유저 권한 Admin 으로 변경
     public void updateAdminRole(Long userid) {
         User user = userRepository.findById(userid).orElseThrow(
                 () -> new NullPointerException("해당 유저가 존재하지 않습니다.")
@@ -54,6 +59,7 @@ public class AdminService {
         userRepository.save(user);
     }
 
+    // 게시글 수정
     public void updatePost(Long postid, PostRequestDto postRequestDto) {
         Post post = postRepository.findById(postid).orElseThrow(
                 () -> new NullPointerException("해당 게시글이 존재하지 않습니다.")
@@ -61,16 +67,19 @@ public class AdminService {
         post.update(postRequestDto);
     }
 
+    // 댓글 수정
     public void updateComment(Long postid, Long commentid, CommentRequestDto commentRequestDto) {
         Comment comment = commentRepository.findByIdAndPostId(commentid, postid);
         comment.update(commentRequestDto);
     }
 
+    // 유저 삭제
     public ApiResponseDto deleteUser(Long userid) {
         userRepository.deleteById(userid);
         return new ApiResponseDto("삭제가 완료되었습니다.",200);
     }
 
+    // 게시글 삭제
     public ApiResponseDto deletePost(Long postid) {
         Post post = postRepository.findById(postid).orElseThrow(
                 () -> new NullPointerException("선택하신 게시물은 존재하지 않습니다.")
@@ -79,6 +88,7 @@ public class AdminService {
         return new ApiResponseDto("삭제가 완료되었습니다.",200);
     }
 
+    // 댓글 삭제
     public ApiResponseDto deleteComment(Long postid, Long commentid) {
         Comment comment = commentRepository.findByIdAndPostId(commentid, postid);
         commentRepository.delete(comment);
