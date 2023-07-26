fabdbedcd83ecf2d343b36526a5aa86ce3094812
VerdureKo <verdure.ko@gmail.com>
2023-07-26 11:29:37 +0900

    이미지 삽입 복구

--- src/main/java/com/sparta/outsideworld/dto/PostRequestDto.java
+++ src/main/java/com/sparta/outsideworld/dto/PostRequestDto.java
@@ -2,7 +2,6 @@ package com.sparta.outsideworld.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 public class PostRequestDto {
@@ -12,5 +11,5 @@ public class PostRequestDto {
     @NotBlank(message = "필수 입력 값입니다.")
     private String contents; // 게시글 내용
 
-    private String image;	// 프론트에서 default 이미지 부탁합니다
+    private String image;
 }
